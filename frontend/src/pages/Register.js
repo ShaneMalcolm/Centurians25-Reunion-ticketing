6c68a9d78bee4f6abe5ab9d0200b7fadfed55c4a
ShaneMalcolm <shanemalcolm02@gmail.com>
2025-12-05 12:27:53 +0530

    Warning added to register page

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -111,15 +111,20 @@ export default function Register() {
 </div>
 
           <div>
-            <label className="block mb-1 font-medium">Email</label>
-            <input
-              name="email"
-              placeholder="Enter your email"
-              onChange={handleChange}
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            />
-          </div>
+  <label className="block mb-1 font-medium">Email</label>
+  <input
+    name="email"
+    placeholder="Enter your email"
+    onChange={handleChange}
+    className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+    required
+  />
+  <p className="text-sm font-semibold mt-1 bg-yellow-100 text-red-700 p-2 rounded">
+  ⚠️ Please use a valid and working email address — your ticket will be issued to this email.
+</p>
+
+
+</div>
 
           <div>
             <label className="block mb-1 font-medium">Password</label>
